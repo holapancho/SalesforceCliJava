13a05c74bd2ee420f7e59f1e63e95830a76a87ea
Francisco Rojas <francisco.rojas@advendio.com>
2020-08-19 21:10:54 -0400

    add unit test

--- src/main/java/cl/holapancho/clijv/command/MyCommand.java
+++ src/main/java/cl/holapancho/clijv/command/MyCommand.java
@@ -5,11 +5,15 @@ import java.util.concurrent.Callable;
 import org.springframework.stereotype.Component;
 
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Component
 @Command(name = "mycommand", 
     mixinStandardHelpOptions = true)
 public class MyCommand implements Callable<Integer> {
+    @Option(names = "-x", description = "optional option")
+    public String x;
+    
     @Override
     public Integer call() {
         System.out.println("Hello in Spanish is Hola!");
--- src/test/java/cl/holapancho/clijv/ClijvApplicationTests.java
+++ src/test/java/cl/holapancho/clijv/ClijvApplicationTests.java
@@ -1,13 +1,31 @@
 package cl.holapancho.clijv;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ClijvApplicationTests {
+import cl.holapancho.clijv.command.MyCommand;
+import picocli.CommandLine;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = NONE, classes = ClijvApplication.class)
+public class ClijvApplicationTests {
+
+    @Autowired
+    CommandLine.IFactory factory;
+
+    @Autowired
+    MyCommand myCommand;
 
 	@Test
-	void contextLoads() {
-	}
-
+    public void testParsingCommandLineArgs() {
+        CommandLine.ParseResult parseResult = new CommandLine(myCommand, factory)
+                .parseArgs("-x", "abc");
+        assertEquals("abc", myCommand.x);
+    }
 }
