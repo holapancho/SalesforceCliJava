370bae3bb9ca307233393b0d7f8ed32262c9afde
Francisco Rojas <francisco.rojas@advendio.com>
2020-08-19 20:09:17 -0400

    Innitial

---
+++ src/main/java/cl/holapancho/clijv/ClijvApplication.java
@@ -0,0 +1,13 @@
+package cl.holapancho.clijv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClijvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClijvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/holapancho/clijv/ClijvApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.holapancho.clijv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClijvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
